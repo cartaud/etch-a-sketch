3f24157c18f713c587f82c54f65fe763ea05c6a5
Chadd <chadd_artaud@cox.net>
2022-03-30 14:21:20 -0700

    added event listener to create drawing effect

--- script.js
+++ script.js
@@ -1,9 +1,22 @@
 const container = document.querySelector('.screen');
+
+
 const dim = 31.25;
+
 for (let i=0; i<256 ; i++){
     const sqr = document.createElement('div');
     sqr.classList.add('square');
     sqr.style.width = `${dim}px`;
     sqr.style.height = `${dim}px`;
     container.append(sqr);
+}
+
+
+const squares = document.querySelectorAll('.square')
+for (let i=0; i<squares.length;i++){
+    squares[i].addEventListener('mouseover', changeColor)
+}
+
+function changeColor() {
+    this.classList.add('red')
 }
