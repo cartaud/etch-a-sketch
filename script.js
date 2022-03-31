39d227c40d35ac0b7190003a8e977e00630343fa
Chadd <chadd_artaud@cox.net>
2022-03-31 11:31:05 -0700

    orginized toggles into single function

--- script.js
+++ script.js
@@ -11,6 +11,7 @@ let dim = (500/size); // dimension for a single square
 for (let i=0; i<total ; i++){
     const sqr = document.createElement('div');
     sqr.classList.add('square');
+    sqr.addEventListener('mouseover', blackFunc);
     sqr.style.width = `${dim}px`;
     sqr.style.height = `${dim}px`;
     container.append(sqr);
@@ -18,7 +19,7 @@ for (let i=0; i<total ; i++){
 
 slider.oninput = function () {
     output.innerHTML = this.value;
-    clearToggle();
+    change(3);
     reSize(this.value);
 }
 
@@ -39,45 +40,18 @@ function reSize(newSize) {
         sqr.style.height = `${dim}px`;
         container.append(sqr);
     }
-
-    
 }
 
-//creates a list of every element with class = square
-const squares = document.querySelectorAll('.square')
 
-//Initializes the starting color to be black 
-for (let i=0; i<squares.length;i++){
-    squares[i].addEventListener('mouseover', blackFunc)
-}
 
-//Will change draw color back to black on button click
-function blackToggle() {
-    for (let i=0; i<squares.length;i++){
-        squares[i].removeEventListener('mouseover', rainbowFunc)
-        squares[i].addEventListener('mouseover', blackFunc)
-    }
-}
+
+
+
+
 
 //on mouse over, the squares background color changes to black
 function blackFunc() {
     this.style.backgroundColor = 'black';
-}
-
-
-
-
-
-//attempting to create rainbow effect WIP
-const rainbowArr = ['red', 'blue', 'green', 'pink', 'purple', 'orange', 'yellow'];
-let rand = Math.floor(Math.random() * rainbowArr.length)
-
-
-function rainbowToggle() {
-    for (let i=0; i<squares.length;i++){
-        squares[i].removeEventListener('mouseover', blackFunc)
-        squares[i].addEventListener('mouseover', rainbowFunc)
-    }
 }
 
 function rainbowFunc() {
@@ -86,6 +60,55 @@ function rainbowFunc() {
 }
 
 
+
+//attempting to create rainbow effect WIP
+const rainbowArr = ['red', 'blue', 'green', 'pink', 'purple', 'orange', 'yellow'];
+let rand = Math.floor(Math.random() * rainbowArr.length)
+
+
+
+function change(value) {
+    //creates a list of every element with class = square
+    const squares = document.querySelectorAll('.square');
+
+    if (value==1) {
+        for (let i=0; i<squares.length;i++){
+            squares[i].removeEventListener('mouseover', rainbowFunc)
+            squares[i].addEventListener('mouseover', blackFunc)
+        }
+    }
+    else if (value==2) {
+        for (let i=0; i<squares.length;i++){
+            squares[i].removeEventListener('mouseover', blackFunc)
+            squares[i].addEventListener('mouseover', rainbowFunc)
+        }
+    }
+    else if (value==3) {
+        for (let i=0; i<squares.length;i++){
+            squares[i].style.backgroundColor = 'white';
+            squares[i].addEventListener('mouseover', blackFunc);
+        }
+    }
+}
+
+
+
+/*
+//Will change draw color back to black on button click
+function blackToggle() {
+    for (let i=0; i<squares.length;i++){
+        squares[i].removeEventListener('mouseover', rainbowFunc)
+        squares[i].addEventListener('mouseover', blackFunc)
+    }
+}
+
+function rainbowToggle() {
+    for (let i=0; i<squares.length;i++){
+        squares[i].removeEventListener('mouseover', blackFunc)
+        squares[i].addEventListener('mouseover', rainbowFunc)
+    }
+}
+
 function clearToggle() {
     for (let i=0; i<squares.length;i++){
         squares[i].style.backgroundColor = 'white';
@@ -93,31 +116,8 @@ function clearToggle() {
     }
 }
 
-
-
-
-/*
-function change(value) {
-    for (let i=0; i<squares.length;i++){
-        squares[i].removeEventListener('mouseover', changeColor)
-        squares[i].removeEventListener('mouseover', blackFunc)
-        squares[i].addEventListener('mouseover', rainbowFunc)
-    }
-    if (value=1) {
-        for (let i=0; i<squares.length;i++){
-            squares[i].addEventListener('mouseover', blackFunc)
-        }
-    }
-    else if (value=2) {
-        for (let i=0; i<squares.length;i++){
-            squares[i].addEventListener('mouseover', rainbowFunc)
-        }
-    }
-    else if (value=3) {
-        for (let i=0; i<squares.length;i++){
-            squares[i].style.backgroundColor = 'white';
-        }
-    }
+//Initializes the starting color to be black 
+for (let i=0; i<squares.length;i++){
+    squares[i].addEventListener('mouseover', blackFunc)
 }
-
 */
